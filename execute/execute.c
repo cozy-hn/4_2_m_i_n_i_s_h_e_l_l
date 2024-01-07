ca8c56bfec95875342731e8b067709095253d7a6
jonique98 <tnals9899@naver.com>
2024-01-07 18:52:16 +0900

    execute

--- execute/execute.c
+++ execute/execute.c
@@ -144,8 +144,6 @@ int	execute(t_arg *arg)
 	int		status;
 	t_list1	*lst;
 
-	status = 0;
-
 	lst = arg->lst;
 	while (lst)
 	{
@@ -165,41 +163,6 @@ int	execute(t_arg *arg)
 		lst = lst->next;
 	}
 	return (status);
-}
-
-t_list1 *mock_lst()
-{
-	t_list1  *lst;
-	t_list1  *tmp;
-
-	lst = malloc(sizeof(t_list1));
-	lst->fd_in = -1;
-	lst->fd_out = -1;
-	lst->cmd = malloc(sizeof(char *) * 3);
-	lst->cmd[0] = strdup("ls");
-	lst->cmd[1] = strdup("-l");
-	lst->cmd[2] = NULL;
-	lst->prev_pipe = -1;
-	lst->next = malloc(sizeof(t_list1));
-	tmp = lst->next;
-	tmp->fd_in = -1;
-	tmp->fd_out = -1;
-	tmp->cmd = malloc(sizeof(char *) * 3);
-	tmp->cmd[0] = strdup("grep");
-	tmp->cmd[1] = strdup("a");
-	tmp->cmd[2] = NULL;
-	tmp->prev_pipe = -1;
-	tmp->next = malloc(sizeof(t_list1));
-	tmp = tmp->next;
-	tmp->fd_in = -1;
-	tmp->fd_out = -1;
-	tmp->cmd = malloc(sizeof(char *) * 3);
-	tmp->cmd[0] = strdup("wc");
-	tmp->cmd[1] = strdup("-l");
-	tmp->cmd[2] = NULL;
-	tmp->prev_pipe = -1;
-	tmp->next = NULL;
-	return (lst);
 }
 
 void	is_valid_command(t_arg *arg, char **cmd)
@@ -279,6 +242,40 @@ int init_arg(t_arg *arg, char **env)
 	get_path(arg, env);
 	arg->env = env;
 	return (0);
+}
+t_list1 *mock_lst()
+{
+	t_list1  *lst;
+	t_list1  *tmp;
+
+	lst = malloc(sizeof(t_list1));
+	lst->fd_in = -1;
+	lst->fd_out = -1;
+	lst->cmd = malloc(sizeof(char *) * 3);
+	lst->cmd[0] = strdup("ls");
+	lst->cmd[1] = strdup("-l");
+	lst->cmd[2] = NULL;
+	lst->prev_pipe = -1;
+	lst->next = malloc(sizeof(t_list1));
+	tmp = lst->next;
+	tmp->fd_in = -1;
+	tmp->fd_out = -1;
+	tmp->cmd = malloc(sizeof(char *) * 3);
+	tmp->cmd[0] = strdup("grep");
+	tmp->cmd[1] = strdup("a");
+	tmp->cmd[2] = NULL;
+	tmp->prev_pipe = -1;
+	tmp->next = malloc(sizeof(t_list1));
+	// tmp = tmp->next;
+	// tmp->fd_in = -1;
+	// tmp->fd_out = -1;
+	// tmp->cmd = malloc(sizeof(char *) * 3);
+	// tmp->cmd[0] = strdup("wc");
+	// tmp->cmd[1] = strdup("-l");
+	// tmp->cmd[2] = NULL;
+	// tmp->prev_pipe = -1;
+	tmp->next = NULL;
+	return (lst);
 }
 
 int main(int ac, char **av, char **env)
