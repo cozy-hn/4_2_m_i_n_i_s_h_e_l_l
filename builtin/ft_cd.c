e1d237b86c16f084f8228a3ea87aeb0388be8d09
jonique98 <tnals9899@naver.com>
2024-01-08 18:05:28 +0900

    env

--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: sumjo <sumjo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 19:27:37 by sumjo             #+#    #+#             */
-/*   Updated: 2024/01/08 21:48:13 by sumjo            ###   ########.fr       */
+/*   Updated: 2024/01/08 21:48:40 by sumjo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ typedef struct s_arg
 int main(int ac, char **av)
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	
 }
 =======
@@ -61,10 +62,31 @@ int main(int ac, char **av)
 	if (chdir(av[1]) == 0)
 		printf("Directory changed\n");
 	else
+=======
+	char *line;
+
+	ac =0;
+	av =0;
+	while (1)
+>>>>>>> env
 	{
-		printf("not a directory\n");
-		exit(1);
+		line = readline("minishell$ ");
+		if (access(line, F_OK) == 0)
+			printf("file exist\n");
+		else
+			printf("file not exist\n");
+		if (access(line, R_OK) == 0)
+			printf("file is executable\n");
+		else
+			printf("file is not executable\n");
+		if (chdir(line) == 0)
+			printf("chdir success\n");
+		else
+			printf("chdir fail\n");
 	}
 }
+<<<<<<< HEAD
 
 >>>>>>> cd test
+=======
+>>>>>>> env
