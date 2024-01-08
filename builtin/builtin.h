7ff68eca652372ec570e08d0bf9dbc9e40f799c2
jonique98 <tnals9899@naver.com>
2024-01-08 20:10:42 +0900

    export

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -4,22 +4,25 @@
 #define OPTION  1
 #define NO_OPTION  0
 
-#include "../lib/libft.h"
+#include "../libft/libft.h"
+#include "../minishell.h"
 
 char	**ft_dup_env(char **env);
 int		ft_env(char **env);
 int		ft_echo(char **args);
 int		ft_free_arr(char **arr);
-int		ft_print_env(char **env);
+int	ft_print_env(char **env);
 char	**ft_sort_env(char **env);
 int		has_char(char *str, char c);
 int		env_len(char *env);
-char	**set_env(char **env, char *str);
-int		ft_export(char ***env, char **str);
+// char	**set_env(char **env, char *str);
+// int		ft_export(char ***env, char **str);
 int		same_env(char *env, char *str);
 int		throw_error(char *cmd, char *str, char *msg);
 int		modify_env(char **str);
-
+char	*ft_strndup(char *s, char *end);
+int		make_env_lst(t_arg *arg, char **env);
+char	**env_lst_to_arr(t_env *env);
 
 
 #endif
--- builtin/ft_cd.c
+++ builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: sumjo <sumjo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 19:27:37 by sumjo             #+#    #+#             */
-/*   Updated: 2024/01/08 16:46:09 by sumjo            ###   ########.fr       */
+/*   Updated: 2024/01/08 18:07:19 by sumjo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,24 +38,5 @@ typedef struct s_arg
 
 int main(int ac, char **av)
 {
-	char *line;
-
-	ac =0;
-	av =0;
-	while (1)
-	{
-		line = readline("minishell$ ");
-		if (access(line, F_OK) == 0)
-			printf("file exist\n");
-		else
-			printf("file not exist\n");
-		if (access(line, R_OK) == 0)
-			printf("file is executable\n");
-		else
-			printf("file is not executable\n");
-		if (chdir(line) == 0)
-			printf("chdir success\n");
-		else
-			printf("chdir fail\n");
-	}
+	
 }
