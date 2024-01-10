56887df7c99166bc0c04b4a91f4938ae9bb04808
jonique98 <tnals9899@naver.com>
2024-01-10 20:11:39 +0900

    test code main

--- execute/execute.c
+++ execute/execute.c
@@ -6,66 +6,11 @@
 /*   By: sumjo <sumjo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 20:06:22 by sumjo             #+#    #+#             */
-/*   Updated: 2024/01/10 18:39:19 by sumjo            ###   ########.fr       */
+/*   Updated: 2024/01/10 20:10:57 by sumjo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minishell.h"
 #include "execute.h"
-
-int	run_execve(t_lst *lst, t_arg *arg)
-{
-	char	**cmd;
-
-	cmd = return_commands(arg, lst->cmd);
-	return (execve(cmd[0], cmd, env_lst_to_arr(arg->env)));
-}
-
-int	is_builtin(t_lst *lst)
-{
-	if (ft_strncmp(lst->cmd[0], "echo", 4) == 0)
-		return (1);
-	else if (ft_strncmp(lst->cmd[0], "cd", 2) == 0)
-		return (1);
-	else if (ft_strncmp(lst->cmd[0], "pwd", 3) == 0)
-		return (1);
-	else if (ft_strncmp(lst->cmd[0], "export", 6) == 0)
-		return (1);
-	else if (ft_strncmp(lst->cmd[0], "unset", 5) == 0)
-		return (1);
-	else if (ft_strncmp(lst->cmd[0], "env", 3) == 0)
-		return (1);
-	else if (ft_strncmp(lst->cmd[0], "exit", 4) == 0)
-		return (1);
-	return (0);
-}
-
-int	run_builtin(t_lst *lst, t_arg *arg)
-{
-	if (ft_strncmp(lst->cmd[0], "echo", 4) == 0)
-		return (ft_echo(lst->cmd));
-	else if (ft_strncmp(lst->cmd[0], "cd", 2) == 0)
-		return (ft_cd(arg, lst->cmd));
-	else if (ft_strncmp(lst->cmd[0], "pwd", 3) == 0)
-		return (ft_pwd());
-	else if (ft_strncmp(lst->cmd[0], "export", 6) == 0)
-		return (ft_export(arg, lst->cmd));
-	else if (ft_strncmp(lst->cmd[0], "unset", 5) == 0)
-		return (ft_unset(arg, lst->cmd));
-	else if (ft_strncmp(lst->cmd[0], "env", 3) == 0)
-		return (ft_env(arg->env));
-	else if (ft_strncmp(lst->cmd[0], "exit", 4) == 0)
-		return (ft_exit(arg, lst->cmd));
-	return (0);
-}
-
-int	execute(t_lst *lst, t_arg *arg)
-{
-	if (is_builtin(lst))
-		return (run_builtin(lst, arg));
-	else
-		return (run_execve(lst, arg));
-}
 
 void	run_middle(t_lst *lst, t_arg *arg, int *pipe_fd)
 {
@@ -141,29 +86,37 @@ int	run_last(t_lst *lst, t_arg *arg)
 	return (pid);
 }
 
+void	executor_helper(t_lst *lst, t_arg *arg, int *status)
+{
+	int	pipe_fd[2];
+	int	pid;
+
+	if (!lst->next)
+	{
+		pid = run_last(lst, arg);
+		waitpid(pid, status, 0);
+	}
+	else
+	{
+		pipe(pipe_fd);
+		if (lst->prev_pipe == -1)
+			run_first(lst, arg, pipe_fd);
+		else
+			run_middle(lst, arg, pipe_fd);
+	}
+}
+
 int	executor(t_arg *arg)
 {
-	int		pid;
 	int		status;
 	t_lst	*lst;
-	int		pipe_fd[2];
 
+	if (is_builtin(arg->lst) && arg->lst->next == NULL)
+		return (run_builtin(arg->lst, arg));
 	lst = arg->lst;
 	while (lst)
 	{
-		if (!lst->next)
-		{
-			pid = run_last(lst, arg);
-			waitpid(pid, &status, 0);
-		}
-		else
-		{
-			pipe(pipe_fd);
-			if (lst->prev_pipe == -1)
-				run_first(lst, arg, pipe_fd);
-			else
-				run_middle(lst, arg, pipe_fd);
-		}
+		executor_helper(lst, arg, &status);
 		lst = lst->next;
 	}
 	return (status);
