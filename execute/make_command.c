b9a659d89fc9659a6cbfdd63be4363330e1d36a9
jonique98 <tnals9899@naver.com>
2024-01-26 11:38:01 +0900

    plz plz plz

--- execute/make_command.c
+++ execute/make_command.c
@@ -6,7 +6,7 @@
 /*   By: sumjo <sumjo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 19:48:59 by sumjo             #+#    #+#             */
-/*   Updated: 2024/01/26 05:58:40 by sumjo            ###   ########.fr       */
+/*   Updated: 2024/01/26 11:30:09 by sumjo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,19 +76,22 @@ char	**return_commands(t_arg	*arg, char **cmd)
 		throw_error(cmd[0], "command not found", 0);
 		exit(127);
 	}
-	if (cmd[0][0] != '/' && cmd[0][0] != '.')
+	if (cmd[0][0] != '/' && cmd[0][0] != '.' && !has_char(cmd[0], '/'))
 		return (parse_commands(arg, cmd));
+	if (access(cmd[0], F_OK) == -1)
+	{
+		throw_error(cmd[0], "No Such file or directory", 0);
+		exit(127);
+	}
+	if (access(cmd[0], X_OK) == -1)
+	{
+		throw_error(cmd[0], "Permission denied", 0);
+		exit(126);
+	}
 	if (is_directory(cmd[0]))
 	{
 		throw_error(cmd[0], 0, "is a directory");
 		exit(126);
-	}
-	if (access(cmd[0], F_OK | X_OK) == -1)
-	{
-		throw_error(cmd[0], 0, strerror(errno));
-		if (errno == 20 || errno == 13)
-			exit(126);
-		exit(127);
 	}
 	return (cmd);
 }
